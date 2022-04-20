470f685de4ed2531c45260c6b9c74863b0ad9a95
githubDo111 <github_do@163.com>
2022-04-20 18:23:42 +0800

    add

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1,7 +1,7 @@
 import { ChainId, CHAINKEY, Token } from '@my/sdk';
 import { BASE_BSC_SCAN_URL, chainKey } from 'config';
 // change chain
-export const chainId = ChainId.ASTR_TESTNET;
+export const chainId = ChainId.ASTR_MAINNET;
 
 const tokens = {
   [CHAINKEY.ASTR]: {
