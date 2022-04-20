33ae5ade2984b3d70bfeb456be1de1356e8244b0
githubDo111 <github_do@163.com>
2022-04-20 11:30:06 +0800

    fee update

--- src/views/Dappstake/hooks/useStakeDApp.ts
+++ src/views/Dappstake/hooks/useStakeDApp.ts
@@ -4,7 +4,7 @@ import { IDappStakingInterface } from 'utils/types';
 export const UseStakeDApp = async (contract: IDappStakingInterface, account: string, value = '0') => {
   const tx = await contract.depositFor(account, {
     value: ethers.utils.parseEther(value),
-    gasLimit: 380000,
+    gasLimit: 1310000,
   });
   if (tx) {
     const receipt = await tx.wait();
--- src/views/Dappstake/hooks/useUnbindDApp.ts
+++ src/views/Dappstake/hooks/useUnbindDApp.ts
@@ -2,7 +2,9 @@ import { ethers } from 'ethers';
 import { IDappStakingInterface } from 'utils/types';
 
 export const UseUnbindDApp = async (contract: IDappStakingInterface, ibASTRAmount: string) => {
-  const tx = await contract.withdraw(ethers.utils.parseEther(ibASTRAmount));
+  const tx = await contract.withdraw(ethers.utils.parseEther(ibASTRAmount), {
+    gasLimit: 1_310_000,
+  });
   const receipt = await tx.wait();
   return receipt.status;
 };
