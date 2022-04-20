8fb9495da6ce37e873c3d5c8dda5c4e2af97d07e
githubDo111 <github_do@163.com>
2022-04-20 17:03:55 +0800

    add

--- src/state/staking/helpers.ts
+++ src/state/staking/helpers.ts
@@ -3,17 +3,21 @@ import erc20ABI from 'config/abi/erc20.json';
 import multicall from 'utils/multicall';
 
 export const fetchUserTokenBalances = async (contractAddress: string, account: string) => {
-  const calls = [
-    {
-      address: contractAddress,
-      name: 'balanceOf',
-      params: [account],
-    },
-  ];
+  try {
+    const calls = [
+      {
+        address: contractAddress,
+        name: 'balanceOf',
+        params: [account],
+      },
+    ];
 
-  const rawTokenBalances = await multicall(erc20ABI, calls);
-  const parsedTokenBalances = rawTokenBalances.map((tokenBalance, index) => {
-    return new BigNumber(tokenBalance).toString();
-  });
-  return parsedTokenBalances[0];
+    const rawTokenBalances = await multicall(erc20ABI, calls);
+    const parsedTokenBalances = rawTokenBalances.map((tokenBalance, index) => {
+      return new BigNumber(tokenBalance).toString();
+    });
+    return parsedTokenBalances[0];
+  } catch (e) {
+    return '0';
+  }
 };
