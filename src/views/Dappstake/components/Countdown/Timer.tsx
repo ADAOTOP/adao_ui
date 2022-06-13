bb7366235074cad9a9f33b19a2771ff22c0d92c7
githubDo111 <github_do@163.com>
2022-06-13 10:23:56 +0800

    add

--- src/views/Dappstake/components/Countdown/Timer.tsx
+++ src/views/Dappstake/components/Countdown/Timer.tsx
@@ -22,25 +22,55 @@ const FlexStyled = styled(Flex)`
   -webkit-text-fill-color: transparent;
 `;
 
-const Wrapper: React.FC<TimerProps> = ({ minutes, hours, days, seconds }) => {
+const Timer: React.FC<TimerProps> = ({ minutes, hours, days, seconds }) => {
   return useMemo(() => {
     return (
       <FlexStyled>
-        <StyledTimerText mb="-4px" scale="xl" mr="4px">
-          {days >= 10 ? days : `0${days}`}:
-        </StyledTimerText>
-        <StyledTimerText mb="-4px" scale="xl" mr="4px">
-          {hours >= 10 ? hours : `0${hours}`}:
-        </StyledTimerText>
-        <StyledTimerText mb="-4px" scale="xl" mr="4px">
-          {minutes >= 10 ? minutes : `0${minutes}`}:
-        </StyledTimerText>
-        <StyledTimerText mb="-4px" scale="xl" mr="4px">
-          {seconds >= 10 ? seconds : `0${seconds}`}
-        </StyledTimerText>
+        <DaysComp days={days} />
+        <HoursComp hours={hours} />
+        <MinComp minutes={minutes} />
+        <SecComp seconds={seconds} />
       </FlexStyled>
     );
   }, [minutes, hours, days, seconds]);
 };
-
-export default Wrapper;
+const DaysComp = ({ days }) => {
+  return useMemo(() => {
+    return (
+      <StyledTimerText mb="-4px" scale="xl" mr="4px">
+        {days >= 10 ? days : `0${days}`}:
+      </StyledTimerText>
+    );
+  }, [days]);
+};
+const HoursComp = ({ hours }) => {
+  return useMemo(() => {
+    return (
+      <StyledTimerText>
+        <h2>{hours >= 10 ? hours : `0${hours}`}</h2>
+        <h4>Hours</h4>
+      </StyledTimerText>
+    );
+  }, [hours]);
+};
+const MinComp = ({ minutes }) => {
+  return useMemo(() => {
+    return (
+      <StyledTimerText>
+        <h2>{minutes >= 10 ? minutes : `0${minutes}`}</h2>
+        <h4>Min</h4>
+      </StyledTimerText>
+    );
+  }, [minutes]);
+};
+const SecComp = ({ seconds }) => {
+  return useMemo(() => {
+    return (
+      <StyledTimerText>
+        <h2>{seconds >= 10 ? seconds : `0${seconds}`}</h2>
+        <h4>Sec</h4>
+      </StyledTimerText>
+    );
+  }, [seconds]);
+};
+export default Timer;
